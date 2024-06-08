8f751e81092f430b9d2330354a500941f2af59ed
jack0713323 <90665172+jack0713323@users.noreply.github.com>
2024-06-08 13:18:18 +0800

    Update train.py

--- tensorflow_code/train.py
+++ tensorflow_code/train.py
@@ -2,7 +2,7 @@ from __future__ import division
 from __future__ import print_function
 
 import time
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 from utils import *
 from models import GCN, MLP,HGCN
